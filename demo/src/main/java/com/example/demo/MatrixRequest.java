1f867acdab465ed703becdfef6c13e82b89bb85f
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-11-19 12:21:19 -0300

    Doing the request from client

---
+++ demo/src/main/java/com/example/demo/MatrixRequest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class MatrixRequest {
+    private int[][] matrix;
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int [][] matrix) {
+        this.matrix = matrix;
+    }
+}
